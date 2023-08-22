3da70fe840cb99ab2b146aa31283b0eb00ea05d0
Neha <96060160+hegdeneha02@users.noreply.github.com>
2023-08-22 21:48:33 +0530

    Update index

--- libsystem/admin/index.php
+++ libsystem/admin/index.php
@@ -1,118 +1,50 @@
-<?php include 'includes/session.php'; ?>
 <?php
-	$where = '';
-	if(isset($_GET['category'])){
-		$catid = $_GET['category'];
-		$where = 'WHERE category_id = '.$catid;
-	}
+  	session_start();
+  	if(isset($_SESSION['admin'])){
+    	header('location:home.php');
+  	}
 ?>
 <?php include 'includes/header.php'; ?>
-<body class="hold-transition skin-blue layout-top-nav">
-<div class="wrapper">
+<body class="hold-transition login-page">
  <nav>
-         <a href="index.php"><img src="jssatebimg1_50.png"></a>
-        </nav>
-
-	<?php include 'includes/navbar.php'; ?>
-        
-	 
-	  <div class="content-wrapper">
-	    <div class="container">
-
-	      <!-- Main content -->
-	      <section class="content">
-	        <div class="row">
-	        	<div class="col-sm-8 col-sm-offset-2">
-	        		<?php
-	        			if(isset($_SESSION['error'])){
-	        				echo "
-	        					<div class='alert alert-danger'>
-	        						".$_SESSION['error']."
-	        					</div>
-	        				";
-	        				unset($_SESSION['error']);
-	        			}
-	        		?>
-	        		<div class="box">
-	        			<div class="box-header with-border">
-	        				<div class="input-group">
-				                <input type="text" class="form-control input-lg" id="searchBox" placeholder="Search for ISBN, Title or Author">
-				                <span class="input-group-btn">
-				                    <button type="button" class="btn btn-primary btn-flat btn-lg"><i class="fa fa-search"></i> </button>
-				                </span>
-				            </div>
-	        			</div>
-	        			<div class="box-body">
-	        				<div class="input-group col-sm-5">
-				                <span class="input-group-addon">Category:</span>
-				                <select class="form-control" id="catlist">
-				                	<option value=0>ALL</option>
-				                	<?php
-				                		$sql = "SELECT * FROM category";
-				                		$query = $conn->query($sql);
-				                		while($catrow = $query->fetch_assoc()){
-				                			$selected = ($catid == $catrow['id']) ? " selected" : "";
-				                			echo "
-				                				<option value='".$catrow['id']."' ".$selected.">".$catrow['name']."</option>
-				                			";
-				                		}
-				                	?>
-				                </select>
-				             </div>
-	        				<table class="table table-bordered table-striped" id="booklist">
-			        			<thead>
-			        				<th>ISBN</th>
-			        				<th>Title</th>
-			        				<th>Author</th>
-			        				<th>Status</th>
-			        			</thead>
-			        			<tbody>
-			        			<?php
-			        				$sql = "SELECT * FROM books $where";
-			        				$query = $conn->query($sql);
-			        				while($row = $query->fetch_assoc()){
-			        					$status = ($row['status'] == 0) ? '<span class="label label-success">available</span>' : '<span class="label label-danger">not available</span>';
-			        					echo "
-			        						<tr>
-			        							
-			        							<td>".$row['isbn']."</td>
-			        							<td>".$row['title']."</td>
-			        							<td>".$row['author']."</td>
-			        							<td>".$status."</td>
-			        						</tr>
-			        					";
-			        				}
-			        			?>
-			        			</tbody>
-			        		</table>
-	        			</div>
-	        		</div>
-	        	</div>
-	        </div>
- <div class="text-center">
-	  <button onclick="window.print()" class="btn btn-primary">Print</button>
-	  </div>
-	      </section>
-	     
-	    </div>
-	  </div>
+	     <a href="index.php"><img src="jssatebimg1_50.png"></a>
+		</nav>
+<div class="login-box">
+  	<div class="login-logo">
+  		<b>JSSATEB</b>
+  	</div>
   
-  	<?php include 'includes/footer.php'; ?>
-</div>
+  	<div class="login-box-body">
+    	<p class="login-box-msg">Sign in to start your session</p>
 
-<?php include 'includes/scripts.php'; ?>
-<script>
-$(function(){
-	$('#catlist').on('change', function(){
-		if($(this).val() == 0){
-			window.location = 'index.php';
-		}
-		else{
-			window.location = 'index.php?category='+$(this).val();
-		}
-		
-	});
-});
-</script>
+    	<form action="login.php" method="POST">
+      		<div class="form-group has-feedback">
+        		<input type="text" class="form-control" name="username" placeholder="input Username" required autofocus>
+        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
+      		</div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" name="password" placeholder="input Password" required>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+      		<div class="row">
+    			<div class="col-xs-4">
+          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+        		</div>
+      		</div>
+    	</form>
+  	</div>
+  	<?php
+  		if(isset($_SESSION['error'])){
+  			echo "
+  				<div class='callout callout-danger text-center mt20'>
+			  		<p>".$_SESSION['error']."</p> 
+			  	</div>
+  			";
+  			unset($_SESSION['error']);
+  		}
+  	?>
+</div>
+	
+<?php include 'includes/scripts.php' ?>
 </body>
 </html>
